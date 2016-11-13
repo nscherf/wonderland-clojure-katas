7d18bb3c2d1d0449c135e0398e636a932f6f0804
Nico Scherf <nscherf@googlemail.com>
2016-11-13 21:10:05 +0100

    how to find wonderland-number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,24 @@
 (ns wonderland-number.finder)
 
+(defn same-digits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))
+        ]
+    (= s1 s2))
+  )
+
+(defn enough-digits? [n]
+  (= 6 (count (str n))))
+
+(defn wonderland-number? [n]
+  (and
+   (enough-digits? n)
+   (every? identity (map #(same-digits? n (* n %)) [2 3 4 5 6]))
+   )
+  )
+
 (defn wonderland-number []
   ;; calculate me
-  42)
+  (first (filter wonderland-number? (range 100000 1000000)))
+  )
+
